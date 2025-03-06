29dbd7eeabd9156687f3fc47b2891bedfb2f53c7
Jade <jade.ramos@student.ateneo.edu>
2025-03-06 12:33:43 +0800

    Created homepage

--- kyusiyasat/kyusiyasat/urls.py
+++ kyusiyasat/kyusiyasat/urls.py
@@ -17,6 +17,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from .views import HomePageView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', HomePageView.as_view(), name='home'),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('user_management/', include('user_management.urls', namespace='user_management')),
 ]
