938577c5a893e2c8a248ee17f5c2d149154c592c
gianvalen <157687026+gianvalen@users.noreply.github.com>
2025-03-06 15:32:07 +0800

    Added my profile page

--- kyusiyasat/user_management/admin.py
+++ kyusiyasat/user_management/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
+from .models import Profile
 
-# Register your models here.
+# Inline Profile model inside UserAdmin
+class ProfileInline(admin.StackedInline):
+    model = Profile
+    can_delete = False
+    verbose_name_plural = 'Profile'
+
+# Extend UserAdmin to include Profile
+class CustomUserAdmin(UserAdmin):
+    inlines = [ProfileInline]
+
+# Unregister the original User model and register the new one with Profile
+admin.site.unregister(User)
+admin.site.register(User, CustomUserAdmin)
+
+# Register Profile separately (optional, but useful for debugging)
+admin.site.register(Profile)
--- kyusiyasat/user_management/urls.py
+++ kyusiyasat/user_management/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import register
+from .views import register, my_profile
 
 app_name = 'user_management'
 
 urlpatterns = [
     path('register/', register, name='register'),
+    path('profile/', my_profile, name='my_profile'),
 ]
