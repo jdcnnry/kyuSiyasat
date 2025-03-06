06333a25e4f293f09440eb8defed508693087bed
Jade <jade.ramos@student.ateneo.edu>
2025-03-06 16:57:39 +0800

    Changes in urls and views

--- kyusiyasat/user_management/urls.py
+++ kyusiyasat/user_management/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import register, my_profile
+from .views import register, my_profile, user_redirect_view
 
 app_name = 'user_management'
 
 urlpatterns = [
+    path('home/', user_redirect_view, name='home'),
     path('register/', register, name='register'),
     path('profile/', my_profile, name='my_profile'),
 ]
