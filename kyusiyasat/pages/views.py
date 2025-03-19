1f44e75a56506906df87f0f767d067313a49e88b
Jade <jade.ramos@student.ateneo.edu>
2025-03-19 20:31:21 +0800

    Removed unnecessary specified path

--- kyusiyasat/pages/views.py
+++ kyusiyasat/pages/views.py
@@ -3,12 +3,12 @@ from django.contrib.auth.decorators import login_required
 
 @login_required
 def getting_started_driver(request):
-    return render(request, 'pages/getting_started_driver.html')
+    return render(request, 'getting_started_driver.html')
 
 @login_required
 def getting_started_commuter(request):
-    return render(request, 'pages/getting_started_commuter.html')
+    return render(request, 'getting_started_commuter.html')
 
 @login_required
 def routes_page(request):
-    return render(request, 'pages/routes.html')
+    return render(request, 'routes.html')
