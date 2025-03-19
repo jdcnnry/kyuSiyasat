4dc9b1874300007333deaeb8eb4cdc8bcdf5bb6f
gianvalen <157687026+gianvalen@users.noreply.github.com>
2025-03-20 00:53:55 +0800

    Modified urls

--- kyusiyasat/pages/urls.py
+++ kyusiyasat/pages/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from . import views
+from .views import getting_started_commuter, getting_started_driver, routes_page
 
 app_name = "pages" 
 
 urlpatterns = [
-    path("getting-started_driver/", views.getting_started_driver, name="getting_started_driver"),
-    path("getting-started_commuter/", views.getting_started_commuter, name="getting_started_commuter"),
-    path("routes/", views.routes_page, name="routes"),
+    path("getting-started_driver/", getting_started_driver, name="getting_started_driver"),
+    path("getting-started_commuter/", getting_started_commuter, name="getting_started_commuter"),
+    path("routes/", routes_page, name="routes"),
 ]
